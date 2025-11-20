ea34f452801c16a50ad392e489e154875eb73037
ronidavidi2107 <roni.davidi@post.runi.ac.il>
2025-11-20 11:45:41 +0200

    final 1

--- Algebra.java
+++ Algebra.java
@@ -1,11 +1,6 @@
-// Implements algebraic operations and the square root function without using 
-// the Java operations a + b, a - b, a * b, a / b, a % b, and without calling 
-// Math.sqrt. All the functions in this class operate on int values and
-// return int values.
 
 public class Algebra {
 	public static void main(String args[]) {
-	    // Tests some of the operations
 	    System.out.println(plus(2,3));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
@@ -23,45 +18,137 @@ public class Algebra {
    		System.out.println(sqrt(76123));
 	}  
 
-	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+	int Result = x1;
+	int Counter = x2;
+	    if (Counter > 0) {
+		   while (Counter > 0) {
+			Result ++;
+			Counter --;
+		   }
+		} else {
+			while (Counter < 0) {
+			Result --;
+			Counter ++;	
+			}
+		}
+		return Result;
 	}
 
-	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int Result = x1;
+	    int Counter = x2;
+		if ( Result > 0) {}
+	    if (Counter > 0) {
+		   while (Counter > 0) {
+			Result --;
+			Counter --;
+		   }
+		} else {
+			while (Counter < 0) {
+			Result --;
+			Counter ++;	
+			}
+		} 
+		if ( Result <0) {
+		while (Counter > 0) {
+			Result ++;
+			Counter ++;
+		   }
+		} else {
+			while (Counter < 0) {
+			Result --;
+			Counter --;	
+			}	
+		}
+
+		return Result;
 	}
 
-	// Returns x1 * x2
+	
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int Result = 0;
+		int Time = x2; 
+		int Counter = x1;
+		if (Time > 0) {
+			while (Time >0) {
+				Result = plus(Result,Counter);
+				Time --;
+			} 
+		 } else {
+			while (Time < 0) {
+				Result = minus(Result, Counter);
+				Time ++;
+				
+			}
+		 }
+		 return Result;
+	
 	}
 
-	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
-	}
-
-	// Returns the integer part of x1 / x2 
+		int result = 1;
+		int time = x;
+		int Counter = n;
+			while (Counter>0) {
+			result = times(result, time);
+			Counter --;	
+			}
+             return result;
+		}
+		
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x1 ==x2) {
+			return 1;
+		}
+		if (x1 <x2) {
+			return 0;
+		}
+		int start= 0;
+		int nam= x2;
+		int finish = x1;
+		int result= 0;
+		if (x1 > x2) {
+		while (result + nam <= finish) {
+			start++;
+			result= times(nam, start);
+			
+		} 
 	}
+	return start;
+	 
 
-	// Returns x1 % x2
+	}  
+
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if ( x1 ==x2) {
+			return 0;
+		} 
+		if (x2 > x1) {
+			return x1;
+		} 
+		if (x2 < x1) {
+			int div = div(x1, x2);
+        return x1 - times(x2, div);
+		}
+	    return 0;
 	}	
 
-	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		if (x<= 0) {
+			return 0;
+		}
+		int i = 1;
+		int result = 1;
+			while (result <= x) {
+				if (result == x) {
+					return i;
+				}
+				i++;
+				result = times(i, i);
+
+				}
+
+		return i;
 	}	  	  
 }
